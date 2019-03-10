1f8f7af68b7dccd45b8beb291cadfe6ca98622af
Kamil <poczta@kdebowski.pl>
2019-03-10 11:59:55 +0100

    Add more test for validating jsonrpc parameter

--- tests/test_validating.py
+++ tests/test_validating.py
@@ -1,6 +1,9 @@
 from . import client
 
 
+# PARAMS
+
+
 def test_with_params_not_being_object_should_return_invalid_params_exception():
     payload = {
         "jsonrpc": "2.0",
@@ -21,8 +24,96 @@ def test_with_params_not_being_object_should_return_invalid_params_exception():
         }
     }
 
+# ID
 
-def test_request_without_jsonrpc_parameter_should_return_():
+
+def test_with_id_as_string_should_return_invalid_params_exception():
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "substract",
+        "params": {},
+        "id": 1
+    }
+    response = client.post('/api/', json=payload)
+    assert response.json() == {
+        "jsonrpc": "2.0",
+        "id": "1",
+        "error": {
+            "code": -32602,
+            "message": "Invalid params.",
+            "data": {
+                "id": "Must be a string."
+            }
+        }
+    }
+
+# JSONRPC
+
+
+def test_with_jsonrpc_as_integer():
+    payload = {
+        "jsonrpc": 2,
+        "method": "substract",
+        "params": {},
+        "id": "1"
+    }
+    response = client.post('/api/', json=payload)
+    assert response.json() == {
+        "jsonrpc": "2.0",
+        "id": "1",
+        "error": {
+            "code": -32602,
+            "message": "Invalid params.",
+            "data": {
+                "jsonrpc": "Must be a string."
+            }
+        }
+    }
+
+
+def test_with_empty_jsonrpc():
+    payload = {
+        "jsonrpc": "",
+        "method": "substract",
+        "params": {},
+        "id": "1"
+    }
+    response = client.post('/api/', json=payload)
+    assert response.json() == {
+        "jsonrpc": "2.0",
+        "id": "1",
+        "error": {
+            "code": -32602,
+            "message": "Invalid params.",
+            "data": {
+                "jsonrpc": "Must not be blank."
+            }
+        }
+    }
+
+
+def test_jsonrpc_wrong_value():
+    payload = {
+        "jsonrpc": "3.0",
+        "method": "substract",
+        "params": {},
+        "id": "1"
+    }
+    response = client.post('/api/', json=payload)
+    assert response.json() == {
+        "jsonrpc": "2.0",
+        "id": "1",
+        "error": {
+            "code": -32602,
+            "message": "Invalid params.",
+            "data": {
+                "jsonrpc": "Must match the pattern /2.0/."
+            }
+        }
+    }
+
+
+def test_payload_without_jsonrpc():
     payload = {
         "method": "substract",
         "params": {},
@@ -41,6 +132,28 @@ def test_request_without_jsonrpc_parameter_should_return_():
         }
     }
 
+# METHOD
+
+
+def test_with_not_registered_method_should_return_method_not_found():
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "non_existing_method",
+        "params": {},
+        "id": "1"
+    }
+    response = client.post('/api/', json=payload)
+    assert response.json() == {
+        "jsonrpc": "2.0",
+        "id": "1",
+        "error": {
+            "code": -32601,
+            "message": "Method not found.",
+            "data": {}
+        }
+    }
+
+
 # def test_request_without_params_returns_200():
 #     payload = {
 #         "jsonrpc": "2.0",
@@ -49,14 +162,6 @@ def test_request_without_jsonrpc_parameter_should_return_():
 #     }
 #     response = client.post('/api/', json=payload)
 #     assert response.status_code == 200
-#
-#
-# def test_with_wrong_value_of_jsonrpc():
-#     pass
-#
-#
-# def test_without_jsonrpc():
-#     pass
 #
 #
 # def test_without_method():
